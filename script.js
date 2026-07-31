9e2f66b2e8c7d4e24796bc1d90b4731f8f09acdf
yuji <ehablym416@gmail.com>
2026-08-01 00:46:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => navLinks.classList.remove("open"));
+});
